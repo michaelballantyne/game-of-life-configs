b219e87a637e93b57809cd0b9dde8e8962b42f8d
Michael Ballantyne <michael.ballantyne@gmail.com>
2014-08-12 14:20:22 -0600

    glider progression

---
+++ configmaker/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to configmaker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ configmaker/project.clj
@@ -0,0 +1,19 @@
+(defproject configmaker "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2277"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+              :builds [
+                       {
+                        :source-paths ["src-cljs"]
+                        ;:compiler {
+                                   ;:optimizations :advanced
+                                   ;}
+                        }
+                       ]
+              }
+  )
